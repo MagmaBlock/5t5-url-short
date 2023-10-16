e9624ef5ff848563bccfb86cfa1a517c70329c4b
岩浆块Magma <magmablock@qq.com>
2023-10-16 17:10:13 +0800

    优化 副标题的域名现在为动态获取

--- components/header/Title.vue
+++ components/header/Title.vue
@@ -2,7 +2,7 @@
   <NSpace justify="space-between">
     <div>
       <NH2 class="mb-0">Url shorter 短网址</NH2>
-      <NH4 class="mt-0 opacity-75">5t5.top</NH4>
+      <NH4 class="mt-0 opacity-75">{{ host }}</NH4>
     </div>
     <NuxtLink
       to="https://beian.miit.gov.cn/"
@@ -14,3 +14,11 @@
   </NSpace>
   <NDivider />
 </template>
+
+<script setup>
+const host = ref("...");
+
+onMounted(() => {
+  host.value = window.location.host;
+});
+</script>
